ecee950a66c142e3f40b8c89034e15460c18fe50
Romario Nijim <75387323+Romarionijim@users.noreply.github.com>
2024-02-02 15:18:00 +0200

    added tow more simple routes for cloud hosting

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('Successfully Deployed to EC2 using Terraform!');
 });
 
+app.get('/about', (req, res) => {
+    res.send("Successfully Routed!!!")
+})
+
+app.get('/edit', (req, res) => {
+    res.send("GET edit route!! >>>><<<<<")
+})
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
